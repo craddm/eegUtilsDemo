34107c87c8d9d6ffdd143472ee32553fbe74d0f4
Matt Craddock <monklefish@gmail.com>
2022-04-22 16:46:43 +0100

    Update install.R
    Add additional dependencies

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages("tidyverse")
 install.packages("devtools")
 remotes::install_github("craddm/eegUtils@develop")
-install.packages(c("JADE", "whitening", "ica", "hdf5", "miniUI", "shiny"))
+install.packages(c("JADE", "whitening", "ica", "hdf5", "miniUI", "shiny", "png", "jpg", "Hmisc", "checkmate", "latticeExtra", "Formula", "survival", "rpart", "nnet", "foreign", "viridis", "gridExtra", "htmlTable"))
