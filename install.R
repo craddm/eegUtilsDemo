77cb62c630942077bc5172f6a00a177682b62497
Matt Craddock <monklefish@gmail.com>
2022-04-22 15:01:46 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages("tidyverse")
 install.packages("devtools")
 remotes::install_github("craddm/eegUtils@develop")
-install.packages(c("JADE", "whitening", "ica", "hdf5"))
+install.packages(c("JADE", "whitening", "ica", "hdf5", "miniUI", "shiny"))
