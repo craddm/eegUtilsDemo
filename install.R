a328257154c3d0c201850fc7768678ab60c795cf
craddm <monklefish@gmail.com>
2022-04-26 12:20:00 +0100

    update to more recent R

--- install.R
+++ install.R
@@ -1,4 +1,8 @@
 install.packages("tidyverse")
 install.packages("devtools")
 remotes::install_github("craddm/eegUtils@develop")
-install.packages(c("JADE", "whitening", "ica", "hdf5", "miniUI", "shiny", "png", "jpg", "Hmisc", "checkmate", "latticeExtra", "Formula", "survival", "rpart", "nnet", "foreign", "viridis", "gridExtra", "htmlTable"))
+install.packages(c("JADE", "whitening", "ica", "hdf5", "miniUI",
+                   "shiny", "png", "jpg", "Hmisc", "checkmate",
+                   "latticeExtra", "Formula", "survival", "rpart",
+                   "nnet", "foreign", "viridis", "gridExtra",
+                   "htmlTable"))
